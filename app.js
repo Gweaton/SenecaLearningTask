59c17c335807ec6ffafb3350db899f42f1ca4cf5
George Eaton <george_eaton@hotmail.co.uk>
2018-01-18 11:56:14 +0000

    Add cors module to allow app to allow local interactions via swagger ui

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').load();
 
 let express = require('express');
+let cors = require('cors');
 let app = express();
 
 let mongoose = require('mongoose');
@@ -28,6 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
+app.use(cors());
 
 app.route('/courses/:courseId')
     .get(stat.aggregateStats)
